398de8a02e1ca476d58198eeac0d8065a8b204ed
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-01-17 09:26:39 -0300

    feat: enhance app layout with global styles and product table integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product } from './interfaces/product.interface';
+import { CloudoNixHttpService } from './services/cloudonix-http/cloudonix-http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cloudonix-test';
+export class AppComponent implements OnInit {
+  private cloudonixHttp = inject(CloudoNixHttpService);
+
+  public products: Product[] = [];
+
+  public ngOnInit(): void {
+    this.cloudonixHttp.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ProductTableComponent } from './components/product-table/product-table.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ProductTableComponent,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
