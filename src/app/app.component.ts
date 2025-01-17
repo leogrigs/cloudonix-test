0f231c48a6d54e8ac454165cf072b4c37408777c
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-01-17 20:54:04 -0300

    feat: update product retrieval to use observable and improve product management flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { ProductModalComponent } from './components/product-modal/product-modal.component';
 import { Product } from './interfaces/product.interface';
 import { CloudoNixHttpService } from './services/cloudonix-http/cloudonix-http.service';
+
+// TODO: provide feedback for success and errors
 
 @Component({
   selector: 'app-root',
@@ -13,12 +16,10 @@ export class AppComponent {
   private cloudonixHttp = inject(CloudoNixHttpService);
   private dialog = inject(MatDialog);
 
-  public products: Product[] = [];
+  public products$: Observable<Product[]> = this.cloudonixHttp.getProducts();
 
-  public login(): void {
-    this.cloudonixHttp.getProducts().subscribe((data) => {
-      this.products = data;
-    });
+  public updateProducts(): void {
+    this.products$ = this.cloudonixHttp.getProducts();
   }
 
   public editProduct(product: Product): void {
@@ -29,6 +30,11 @@ export class AppComponent {
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         console.log('Updated product:', result);
+        this.cloudonixHttp.editProduct(product.id, result).subscribe({
+          next: (data) => {
+            this.updateProducts();
+          },
+        });
       }
     });
   }
@@ -46,6 +52,10 @@ export class AppComponent {
   }
 
   public deleteProduct(product: Product): void {
-    console.log('Deleting product:', product);
+    this.cloudonixHttp.deleteProduct(product.id).subscribe({
+      next: () => {
+        this.updateProducts();
+      },
+    });
   }
 }
