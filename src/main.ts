5120061cfd2d992861da1fb551332486263002ae
Leonardo Grigorio Ferreira <leo.grigorio16@gmail.com>
2025-01-19 10:14:36 -0300

    feat: add support for Angular Elements by integrating dynamic profile fields and profile fields components

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+import { Injector, NgModuleRef } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { DynamicProfileFieldsComponent } from './app/components/dynamic-profile-fields/dynamic-profile-fields.component';
+import { ProfileFieldsComponent } from './app/components/profile-fields/profile-fields.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((moduleRef: NgModuleRef<AppModule>) => {
+    const injector: Injector = moduleRef.injector;
+    const profileFieldsElement = createCustomElement(ProfileFieldsComponent, {
+      injector,
+    });
+    const dynamicProfileFields = createCustomElement(
+      DynamicProfileFieldsComponent,
+      { injector }
+    );
+    customElements.define('profile-fields', profileFieldsElement);
+    customElements.define('dynamic-profile-fields', dynamicProfileFields);
+  })
+  .catch((err) => console.error(err));
